e4243221771d619c80c86ebae1b6e7cdf5e109e1
mateusdata <mateuspele2015@gmail.com>
2023-08-12 02:42:37 -0300

    added: return new object

--- api/index.php
+++ api/index.php
@@ -1,21 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-<div>
 <?php
-    $nomes = array("Mateus", "Jhonnata", "Edras", "Caio");
 
-    foreach ($nomes as $nome) {
-        echo "<li>$nome</li>";
-    }
-    ?>
-</div>
-</body>
-</html>
+// Dados fictícios de um usuário
+$usuario = [
+    'id' => 1,
+    'nome' => 'João Silva',
+    'email' => 'joao@example.com'
+];
+
+// Transforma os dados do usuário em JSON e retorna
+header('Content-Type: application/json');
+echo json_encode($usuario);
+
+?>
