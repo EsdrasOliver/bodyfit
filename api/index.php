bfe854611f1f36ef2d1654af2fa03d29c30ffeb7
mateusdata <mateuspele2015@gmail.com>
2023-08-12 03:47:33 -0300

    add return for fronend

--- api/index.php
+++ api/index.php
@@ -1,14 +1,30 @@
 <?php
 
-// Dados fictícios de um usuário
-$usuario = [
-    'id' => 1,
-    'nome' => 'João Silva',
-    'email' => 'joao@example.com'
-];
+$host = 'localhost';  // Ou o endereço IP do servidor local
+$dbname = 'usuarios';
+$username = 'root';  // Substitua pelo seu nome de usuário do banco de dados
+$password = '';   // Substitua pela sua senha do banco de dados
 
-// Transforma os dados do usuário em JSON e retorna
-header('Content-Type: application/json');
-echo json_encode($usuario);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+    // Defina o atributo PDO para lançar exceções em erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Preparar e executar a consulta SELECT
+    $stmt = $pdo->query("SELECT * FROM new_table");
+
+    // Recuperar os resultados como objetos
+    $resultados = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+    // Transformar os resultados em JSON
+    $jsonResultados = json_encode($resultados);
+
+    // Exibir o JSON
+    header('Content-Type: application/json');
+    echo $jsonResultados;
+} catch (PDOException $e) {
+    echo "Erro de conexão: " . $e->getMessage();
+}
 
 ?>
